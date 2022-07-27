eabdfa8b7f0e46c944b3e853da5c6abb4cb0b806
JM Arenas <jm.arenas@interleukin.co>
2022-07-27 15:19:48 +0800

    Corrected Stategies to Strategies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  include Devise::JWT::RevocationStategies::JTIMatcher
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable, :recoverable, :rememberable,
