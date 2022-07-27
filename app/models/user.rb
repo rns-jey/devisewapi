dcb87a85b4926013e76ca53246f1c8c36333199b
JM Arenas <jm.arenas@interleukin.co>
2022-07-27 15:19:14 +0800

    Added the strategy to the model class and configure it accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable, :recoverable, :rememberable,
+         :jwt_authenticatable, jwt_revocation_stategy: self
 end
